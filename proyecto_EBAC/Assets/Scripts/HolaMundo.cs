f9c4192eee804eae3cf12e3edcc8560758d12edc
mmokarzel <107641440+mmokarzel@users.noreply.github.com>
2022-11-07 23:12:37 -0600

    Ejemplo Hola Mundo y Debug

---
+++ proyecto_EBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(" Hola Mundo!!");
+        Debug.Log("Hola Mundo");
+        Debug.LogError("ALgo Salió Mal");
+        Debug.LogWarning("Cuidado");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
